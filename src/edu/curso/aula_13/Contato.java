da0ceb36757c7b53b2b123bf817830fbf4b153bf
Thiago Antenor <thiagoantenor31@gmail.com>
2025-01-29 11:59:31 -0300

    feat: envio da aula 13

---
+++ src/edu/curso/aula_13/Contato.java
@@ -0,0 +1,52 @@
+package edu.curso.aula_13;
+
+import java.time.LocalDate;
+
+public class Contato {
+  private int id = 0;
+  private String nome = "";
+  private String telefone = "";
+  private String email = "";
+  private LocalDate nascimento = LocalDate.now();
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public LocalDate getNascimento() {
+    return nascimento;
+  }
+
+  public void setNascimento(LocalDate nascimento) {
+    this.nascimento = nascimento;
+  }
+
+}
